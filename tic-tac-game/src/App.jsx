94aa59f23fe5ff2ba08418e4357ff53a7cd5fa32
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-24 12:50:26 +0200

    updated the app component

--- tic-tac-game/src/App.jsx
+++ tic-tac-game/src/App.jsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Restart from "./assets/refresh.png";
-
+import Cell from './Cell';
 import './App.css'
 
 function App() {
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("marshmallow");
+  const [winningMsg, setWinningMsg] = useState(null);
+  const [rainIntervalid, setRainIntervalid] = useState(null);
+  const message = "it is now" + go + "'s go.";
+
+  useState(() => {
+    checkScore();
+  }, [cells]);
 
   return (
     <>
-     <h1>hello</h1>
- 
+    
     </>
   )
 }
